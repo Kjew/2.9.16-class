09e1a059146b86f68ac0c212c9bc141c2592f4b0
Gulmira <miramaxeast@yahoo.com>
2016-02-11 18:13:10 -0800

    added dictionary2.py

---
+++ dictionary2.py
@@ -0,0 +1,21 @@
+vocabulary_words = {"bear":"Big animal, lives in woods. Grrrrr", "tiger":"Big cat, lives in jungle, orange fur, stripes, grrrrr.", "lions": "Big cat, lives in plains, rarrrrrrrrr", "owl":"Birdie, lives in a barn or a tree, you know. Hoo Hoo."}
+
+def letter_count(name):
+	print "Name:", name
+	count = {}
+	for letter in name:
+		if letter not in count:
+			count[letter] = 1
+		else:
+			count[letter] += 1
+	return count
+
+
+name = raw_input("Enter your name:")
+print letter_count(name)
+
+prices = { "banana": 4, "apple": 2, "orange": 1.5, "pear": 3 }
+
+def most_expensive():
+	for fruit, price in prices.values():
+		if price 
